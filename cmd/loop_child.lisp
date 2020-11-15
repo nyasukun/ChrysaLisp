22a366737792af75df8e1bc912b7471f1400f428
nyasukun <n@yasukun.org>
2020-11-15 20:04:20 +0900

    .

--- cmd/loop_child.lisp
+++ cmd/loop_child.lisp
@@ -1,12 +1,5 @@
 ;imports
 (import "cmd/vptest.inc")
-;(import "class/lisp.inc")
-;(import "sys/lisp.inc")
-;
-;(import "lib/asm/asm.inc")
-;(make '("cmd/vptest.vp"))
-
-;(ffi monitor "cmd/monitor" 0)
 
 (bind '(parent_mbox input) (mail-read (task-mailbox)))
 	(print "list:" input " vcpuid: " (monitor))
@@ -18,12 +11,12 @@
 	(defq l (filter (rcurry < p) input))
 	(defq r (filter (rcurry > p) input))
 	(mail-send (list (task-mailbox) l)
-		(defq lmbox (open-child "cmd/loop_child.lisp" kn_call_open)))
-		;(defq lmbox (open-child "cmd/loop_child.lisp" kn_call_child))
+		;(defq lmbox (open-child "cmd/loop_child.lisp" kn_call_open)))
+		(defq lmbox (open-child "cmd/loop_child.lisp" kn_call_child)))
 	(defq rtask-mailbox (mail-alloc-mbox))
 	(mail-send (list rtask-mailbox r)
-		(defq rmbox (open-child "cmd/loop_child.lisp" kn_call_open)))
-		;(defq rmbox (open-child "cmd/loop_child.lisp" kn_call_child)))
+		;(defq rmbox (open-child "cmd/loop_child.lisp" kn_call_open)))
+		(defq rmbox (open-child "cmd/loop_child.lisp" kn_call_child)))
 	(defq ol (mail-read (task-mailbox)))
 	(defq our (mail-read rtask-mailbox))
 	(mail-send (cat ol (list p) our) parent_mbox)
